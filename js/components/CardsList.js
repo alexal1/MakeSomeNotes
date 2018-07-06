70cc90885f1a31428461a3fda929a32a4f842c6a
Alexander Mishchenko <5740235@gmail.com>
2018-07-06 18:26:56 +0300

    Create PlusView component and add it to the end of CardsList

--- js/components/CardsList.js
+++ js/components/CardsList.js
@@ -8,18 +8,55 @@ import React, { PureComponent } from "react";
 import CardViewContainer from "../containers/CardViewContainer";
 import Carousel from "react-native-snap-carousel";
 import Globals from "../globals"
+import PlusView from "./PlusView";
+
+type CarouselItem = {
+    key: string,
+    type: string,
+    cardId: ?number
+}
 
 type Props = {
     cardIds: number[]
 }
 
 export default class CardsList extends PureComponent<Props> {
+    CAROUSEL_ITEM_CARD = "CAROUSEL_ITEM_CARD";
+    CAROUSEL_ITEM_PLUS = "CAROUSEL_ITEM_PLUS";
+
+    obtainData = (cardIds: number[]) => {
+        const data = [];
+        cardIds.forEach((cardId, i) => {
+            data.push({
+                key: i.toString(),
+                type: this.CAROUSEL_ITEM_CARD,
+                cardId: cardId
+            })
+        });
+        data.push({
+            key: cardIds.length.toString(),
+            type: this.CAROUSEL_ITEM_PLUS
+        });
+        return data
+    };
+
+    renderItem = (item: CarouselItem) => {
+        switch (item.type) {
+            case this.CAROUSEL_ITEM_CARD: {
+                return <CardViewContainer cardId={item.cardId}/>
+            }
+
+            case this.CAROUSEL_ITEM_PLUS: {
+                return <PlusView createCard={() => console.log("create card")}/>
+            }
+        }
+    };
+
     render() {
         return (
             <Carousel
-                data={this.props.cardIds}
-                renderItem={({item}) => <CardViewContainer cardId={item}/>}
-                keyExtractor={(item) => item.toString()}
+                data={this.obtainData(this.props.cardIds)}
+                renderItem={({item}) => this.renderItem(item)}
                 horizontal={true}
                 sliderWidth={Globals.SCREEN_WIDTH}
                 itemWidth={Globals.SCREEN_WIDTH}
---
+++ js/components/PlusView.js
@@ -0,0 +1,44 @@
+/**
+ * Card with "plus" button to create one more card.
+ *
+ * @flow
+ */
+
+import React, { PureComponent } from "react";
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native'
+import Globals from "../globals"
+
+type Props = {
+    createCard: () => any
+}
+
+export default class PlusView extends PureComponent<Props> {
+
+    render() {
+        return (
+            <View
+                style={styles.root}>
+                <TouchableOpacity
+                    onPress={this.props.createCard}>
+                    <Text
+                        style={styles.text}>
+                        +
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: Globals.STATUS_BAR_HEIGHT()
+    },
+    text: {
+        fontSize: 128,
+        fontWeight: "100"
+    }
+});
